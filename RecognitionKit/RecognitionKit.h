3c45f5f09c94ca59882c491276567a1e36c137f3
Mars <mars09250148@gmail.com>
2024-02-29 11:07:35 +0800

    Initial Commit

---
+++ RecognitionKit/RecognitionKit.h
@@ -0,0 +1,18 @@
+//
+//  RecognitionKit.h
+//  RecognitionKit
+//
+//  Created by 張宮豪 on 2024/2/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RecognitionKit.
+FOUNDATION_EXPORT double RecognitionKitVersionNumber;
+
+//! Project version string for RecognitionKit.
+FOUNDATION_EXPORT const unsigned char RecognitionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecognitionKit/PublicHeader.h>
+
+
